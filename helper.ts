b912b0202f1b1bb00956e2b551299c5444f151cb
Dmytro Nikitiuk <d.nikitiuk@noisyminer.com>
2021-12-27 14:47:09 +0200

    Install & run prettier

--- helper.ts
+++ helper.ts
@@ -1,31 +1,32 @@
-import * as http from 'https';
+import * as http from "https";
 
-const session = '53616c7465645f5f5e40087102b96ea8cd8fbfa1b0684f8ace69cb16bba72d1b8cb70658dc513847903d0a558e2a20d3';
+const session =
+  "53616c7465645f5f5e40087102b96ea8cd8fbfa1b0684f8ace69cb16bba72d1b8cb70658dc513847903d0a558e2a20d3";
 
 export function getInput(day: number, year = 2020): Promise<string> {
   return new Promise((resolve, reject) => {
     const options = {
-      host: 'adventofcode.com',
+      host: "adventofcode.com",
       path: `/${year}/day/${day}/input`,
-      headers: {Cookie: `session=${session}`}
+      headers: { Cookie: `session=${session}` },
     };
 
     const req = http.request(options, (response) => {
-      let str = '';
+      let str = "";
 
-      response.on('data', (chunk: string) => {
+      response.on("data", (chunk: string) => {
         str += chunk;
       });
 
-      response.on('end', () => {
-        if (str.charAt(str.length - 1) === '\n') {
+      response.on("end", () => {
+        if (str.charAt(str.length - 1) === "\n") {
           str = str.slice(0, -1);
         }
         resolve(str);
       });
     });
 
-    req.on('error', (e: unknown) => {
+    req.on("error", (e: unknown) => {
       reject(e);
     });
 
@@ -33,14 +34,28 @@ export function getInput(day: number, year = 2020): Promise<string> {
   });
 }
 
-export function getTestFunction(call: (input: string[]) => number): (data: string[], expected: unknown) => boolean {
+export function getTestFunction(
+  call: (input: string[]) => number
+): (data: string[], expected: unknown) => boolean {
   return (data: string[], expected: unknown) => {
     const actual = call(data);
     if (actual === expected) {
-      console.log('\x1b[32mSuccess test\x1b[0m:\x1b[36m', data, '\x1b[32m', actual, '\x1b[0m');
-    }
-    else {
-      console.log('\x1b[31mFail test\x1b[0m:\x1b[36m', data, '\x1b[0mexpected:\x1b[33m', expected, '\x1b[0mactual:\x1b[31m', actual);
+      console.log(
+        "\x1b[32mSuccess test\x1b[0m:\x1b[36m",
+        data,
+        "\x1b[32m",
+        actual,
+        "\x1b[0m"
+      );
+    } else {
+      console.log(
+        "\x1b[31mFail test\x1b[0m:\x1b[36m",
+        data,
+        "\x1b[0mexpected:\x1b[33m",
+        expected,
+        "\x1b[0mactual:\x1b[31m",
+        actual
+      );
     }
 
     return actual === expected;
